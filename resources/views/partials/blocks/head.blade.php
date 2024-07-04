69c070ed4a38ac8af3ec7098220ed753d079f692
izzoel <ramadhani.zulfahmi@gmail.com>
2024-07-04 09:18:59 +0800

    fix landing navbar

--- resources/views/partials/blocks/head.blade.php
+++ resources/views/partials/blocks/head.blade.php
@@ -13,11 +13,8 @@
                 <i class="fas fa-users"></i>
             </a>
             <a class="navbar-brand" href="{{ url('/') }}">
-                {{-- <a class="site_title"> --}}
                 <img src="{{ asset('images/site.png') }}" alt="" width="300" height="300"
                     class="img-fluid">
-                {{-- </a> --}}
-                {{-- <b style="color: black">kemahasiswaan</b> --}}
                 <span class="text-white p-1 rounded" style="background-color: #09114d">.unbl</span>
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
@@ -25,15 +22,15 @@
                 aria-label="Toggle navigation">
                 <i class="fas fa-bars"></i>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent" style="color: black !important">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-lg-0">
                     <li class="nav-item">
                         <a class="nav-link" href="https://tracer.unbl.ac.id/"
                             style="color: black !important; text-decoration: none;">
-                            <span class=" p-2 pr-4 pl-4 rounded"
+                            <span class="p-2 pr-4 pl-4 rounded"
                                 style="background-color: #FF5733; color: white !important; border: 1px solid #FF5733;"
-                                onmouseover="this.style.backgroundColor='#FFFFFF'; this.style.color='black';this.style.border='1px solid #FF5733';"
-                                onmouseout="this.style.backgroundColor='#FF5733'; this.style.color='white';this.style.borderColor='#FF5733';">
+                                onmouseover="this.style.backgroundColor='#FFFFFF'; this.style.color='black'; this.style.border='1px solid #FF5733';"
+                                onmouseout="this.style.backgroundColor='#FF5733'; this.style.color='white'; this.style.borderColor='#FF5733';">
                                 Tracer
                             </span>
                         </a>
@@ -45,37 +42,23 @@
                                 onmouseout="this.style.color='black'">Informasi</span>
                         </a>
                     </li>
-
                     <li class="nav-item">
                         <a class="nav-link @@about" href="about.html" style="color: black !important">
-                            <span onmouseover="this.style.color='red'" onmouseout="this.style.color='black'">Beasiswa
-                            </span>
+                            <span onmouseover="this.style.color='red'"
+                                onmouseout="this.style.color='black'">Beasiswa</span>
                         </a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link @@prestasi" href="{{ route('prestasi') }}"
                             style="color: black !important">
-                            <span onmouseover="this.style.color='red'" onmouseout="this.style.color='black'">Prestasi
-                            </span>
+                            <span onmouseover="this.style.color='red'"
+                                onmouseout="this.style.color='black'">Prestasi</span>
                         </a>
                     </li>
-                    {{-- <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle @@portfolio @@portfolio-single"
-                            href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false" style="color: black !important">
-                            Prestasi
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item @@portfolio" href="portfolio.html"
-                                    style="color: black !important">Portfolio</a>
-                            </li>
-                            <li><a class="dropdown-item @@portfolio-single" href="portfolio-single.html"
-                                    style="color: black !important">Portfolio single</a></li>
-                        </ul>
-                    </li> --}}
                 </ul>
             </div>
         </div>
+
     </nav>
 </header>
 <!--================== Header Section End ==================-->
