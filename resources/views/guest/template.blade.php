06de04b3e3e152b08806f6cc2629793a293bb9a1
izzoel <ramadhani.zulfahmi@gmail.com>
2025-04-27 17:40:49 +0800

    - fix favicon web

--- resources/views/guest/template.blade.php
+++ resources/views/guest/template.blade.php
@@ -31,7 +31,10 @@
     <!-- Select2 CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css">
-    <!--Favicon-->
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="{{ asset('img/favicon.ico') }}" />
+
     <!-- <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
   <link rel="icon" href="images/favicon.ico" type="image/x-icon"> -->
 
